25022a240ab7d31f45efb0058f71f48178ff0d65
Yulia_Zaitseva <Yulia_Zaitseva@epam.com>
2021-11-10 22:59:59 +0300

    tried to add docker; dockerfiles and docker-compose partially work

---
+++ drivers-service/src/main/java/com/example/Driversservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.Driversservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors()
+                // don't authenticate any request
+                .and().authorizeRequests().antMatchers("/*").permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+}
